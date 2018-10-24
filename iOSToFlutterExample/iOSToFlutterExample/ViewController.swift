3ca091969bc37fa80aa9919b7e201db1e1248dc7
NeoKree <neokree@gmail.com>
2018-10-25 00:19:20 +0200

    Integrate flutter module in iOS project

---
+++ iOSToFlutterExample/iOSToFlutterExample/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  iOSToFlutterExample
+//
+//  Created by Fabio Biola on 24/10/18.
+//  Copyright © 2018 Fabio Biola. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
